823c6ae12248fc79e822147c8853c5fdeba281f0
iasiddiqui <115731094+iasiddiqui@users.noreply.github.com>
2024-09-17 15:22:03 +0530

    wotking on conference card

---
+++ src/components/ConferenceDetails/ConferenceData.jsx
@@ -0,0 +1,17 @@
+// ConferenceData.js
+const ConferenceData = {
+    title: "International Conference on Climate Change and Public Health",
+    theme: "Current Trends and Research on Climate Change and Public Health",
+    date: "July 21-22, 2025",
+    location: "Vancouver, Canada",
+    deadline:"Abstract Submission Opened",
+    buttons: [
+      { text: "Register Now", link: "" },
+      { text: "Abstract Submission", link: "" },
+      { text: "Brochure Download", link: "" },
+      { text: "Tentative Program", link: "" },
+    ],
+  };
+  
+  export default ConferenceData;
+  
---
+++ src/components/ConferenceDetails/ConferenceDetails.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./ConferenceDetails.css";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import ConferenceData from "./ConferenceData";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+const ConferenceDetails = () => {
+  return (
+    <>
+      <div className="conference">
+        <img
+          className="conference-img"
+          src="/pictures/Climate change.jpg"
+          alt="Climate Change"
+        />
+        <div className="conference-box">
+          <div className="conference-hero">
+            <h1 className="conference-hero-h1">
+              {ConferenceData.title}
+            </h1>
+            <p className="conference-hero-p">
+              Theme: {ConferenceData.theme}
+            </p>
+          </div>
+          <div className="conference-hero-box">
+            <CalendarMonthIcon  className="conference-icon"/>
+            <span className="conference-hero-box-span">Dates:</span>
+            <span className="conference-hero-box-span1">
+              {ConferenceData.date}
+            </span>
+          </div>
+          <div className="conference-hero-box">
+            <LocationOnIcon className="conference-icon"/>
+            <span className="conference-hero-box-span">Venue:</span>
+            <span className="conference-hero-box-span1">
+              {ConferenceData.location}
+            </span>
+          </div>
+          <div className="conference-hero-box">
+            <NotificationsIcon className="blinking-icon" />
+            <span className="blinking">
+              {ConferenceData.deadline}
+            </span>
+          </div>
+          <div className="conference-hero-box-button">
+            {ConferenceData.buttons.map((button, index) => (
+              <button key={index} className="conference-button">
+                {button.text}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ConferenceDetails;
