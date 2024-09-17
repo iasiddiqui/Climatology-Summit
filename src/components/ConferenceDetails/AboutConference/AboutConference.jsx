b4d61d8292d12b1cdd7be666d18fe2c62d4ec69a
iasiddiqui <115731094+iasiddiqui@users.noreply.github.com>
2024-09-17 16:06:45 +0530

    about conference

---
+++ src/components/ConferenceDetails/AboutConference/AboutConference.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const AboutConference = () => {
+  return (
+    <>
+      <div className="about-conference-container">
+        <div className="about-conference">
+          <div className="about-conference-img"></div>
+          <img
+            className="about-conference-image-img"
+            src="public\pictures\content-2.jpg"
+          />
+        </div>
+        <div className="about-conference">
+          <div className="about-conference-img"></div>
+          <img
+            className="about-conference-image-img"
+            src="public\pictures\upcoming img 1.jpg"
+          />
+        </div>
+        <div className="about-conference">
+          <div className="about-conference-img"></div>
+          <img
+            className="about-conference-image-img"
+            src="public\pictures\upcoming img 3.jpg"
+          />
+        </div>
+        <div className="about-conference">
+          <div className="about-conference-img"></div>
+          <img
+            className="about-conference-image-img"
+            src="public\pictures\upcoming img 4.jpg"
+          />
+        </div>
+      </div>
+      <div className="about-conferecne-content">
+        <h1 className="about-confernece-h1">
+          About Climate Change Conference 2025
+        </h1>
+        <p className="about-conference-p">
+          We are excited to extend an invitation to the International Conference
+          on Climate Change and Public Health, scheduled for July 21-22, 2025,
+          in Vancouver, Canada. This significant gathering will unite prominent
+          experts, researchers, and policymakers to examine the critical effects
+          of climate change on public health and to discuss effective strategies
+          for response and adaptation. Conference Theme: “Navigating the
+          Intersection: Climate Change, Public Health, and Resilience” Under the
+          theme “Navigating the Intersection: Climate Change, Public Health, and
+          Resilience,” this conference aims to delve into the intricate
+          connections between climate change and health outcomes. Our goal is to
+          investigate innovative solutions, research insights, and policies that
+          can empower communities to adapt to climate-related challenges while
+          enhancing public health resilience.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default AboutConference;
--- src/components/HealthConference/HealthConference.jsx
+++ src/components/HealthConference/HealthConference.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar/navbar'
 import ConferenceDetails from '../ConferenceDetails/ConferenceDetails'
+import AboutConference from '../ConferenceDetails/AboutConference/AboutConference'
 
 const HealthConference = () => {
   return (
     <>
     <Navbar/>
     <ConferenceDetails/>
+    <AboutConference />
     </>
   )
 }
