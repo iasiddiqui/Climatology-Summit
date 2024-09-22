54e9379fb0537bc8ad5fc64f6cf9c7302c3cee38
iasiddiqui <115731094+iasiddiqui@users.noreply.github.com>
2024-09-22 22:52:46 +0530

    register page

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./register.css";
 import PaymentInfo from "./PaymentMethod/PaymentMethod";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Home/Footer/Footer";
 
 function Register() {
   const [title, setTitle] = useState("");
@@ -59,99 +61,143 @@ function Register() {
   };
 
   return (
-    <div className="register-container">
-    <form className="register-form" onSubmit={handleSubmit}>
-      <h2 className="register-title">Personal Details</h2>
-      <div className="register-field">
-        <label className="register-label" htmlFor="title">Title:</label>
-        <select className="register-select" id="title" value={title} onChange={(e) => setTitle(e.target.value)}>
-          <option value="">Select Any</option>
-          <option value="Mr.">Mr.</option>
-          <option value="Mrs.">Mrs.</option>
-          <option value="Ms.">Ms.</option>
-        </select>
-      </div>
-      <div className="register-field">
-        <label className="register-label" htmlFor="firstName">First Name:</label>
-        <input
-          className="register-input"
-          type="text"
-          id="firstName"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          required
+    <>
+      <Navbar />
+      <div className="hero-section">
+        <img
+          className="hero-picture"
+          src="public\pictures\awards.jpg"
+          alt="Hero"
         />
-      </div>
-      <div className="register-field">
-        <label className="register-label" htmlFor="lastName">Last Name:</label>
-        <input
-          className="register-input"
-          type="text"
-          id="lastName"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          required
-        />
-      </div>
-      <div className="register-field">
-        <label className="register-label" htmlFor="email">Email:</label>
-        <input
-          className="register-input"
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div className="register-field">
-        <label className="register-label" htmlFor="phoneNumber">Phone Number:</label>
-        <input
-          className="register-input"
-          type="tel"
-          id="phoneNumber"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          required
-        />
+
+        <div className="overlay">
+          <h1 className="abstract-h1">
+            Register
+          </h1>
+        </div>
       </div>
 
-      <h2 className="register-title">Further Information</h2>
-      <div className="register-field">
-        <label className="register-label" htmlFor="country">Country:</label>
-        <select className="register-select" id="country" value={country} onChange={(e) => setCountry(e.target.value)}>
-          <option value="">Select country</option>
-          <option value="India">India</option>
-          <option value="USA">USA</option>
-        </select>
-      </div>
-     
-      <div className="register-field">
-        <label className="register-label" htmlFor="abstractCategory">Abstract Category:</label>
-        <select
-          className="register-select"
-          id="abstractCategory"
-          value={abstractCategory}
-          onChange={(e) => setAbstractCategory(e.target.value)}
-        >
-          <option value="">Select Category</option>
-          <option value="Technology">Technology</option>
-          <option value="Science">Science</option>
-        </select>
-      </div>
-      <div className="register-field">
-        <label className="register-label" htmlFor="fullAddress">Full Postal Address:</label>
-        <textarea
-          className="register-textarea"
-          id="fullAddress"
-          value={fullAddress}
-          onChange={(e) => setFullAddress(e.target.value)}
-        />
-      </div>
+      <div className="register-container">
+        <form className="register-form" onSubmit={handleSubmit}>
+          <h2 className="register-title">Personal Details</h2>
+          <div className="register-field">
+            <label className="register-label" htmlFor="title">
+              Title:
+            </label>
+            <select
+              className="register-select"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            >
+              <option value="">Select Any</option>
+              <option value="Mr.">Mr.</option>
+              <option value="Mrs.">Mrs.</option>
+              <option value="Ms.">Ms.</option>
+            </select>
+          </div>
+          <div className="register-field">
+            <label className="register-label" htmlFor="firstName">
+              First Name:
+            </label>
+            <input
+              className="register-input"
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="register-field">
+            <label className="register-label" htmlFor="lastName">
+              Last Name:
+            </label>
+            <input
+              className="register-input"
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="register-field">
+            <label className="register-label" htmlFor="email">
+              Email:
+            </label>
+            <input
+              className="register-input"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="register-field">
+            <label className="register-label" htmlFor="phoneNumber">
+              Phone Number:
+            </label>
+            <input
+              className="register-input"
+              type="tel"
+              id="phoneNumber"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              required
+            />
+          </div>
 
-      <PaymentInfo />
-    </form>
-    </div>
+          <h2 className="register-title">Further Information</h2>
+          <div className="register-field">
+            <label className="register-label" htmlFor="country">
+              Country:
+            </label>
+            <select
+              className="register-select"
+              id="country"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+            >
+              <option value="">Select country</option>
+              <option value="India">India</option>
+              <option value="USA">USA</option>
+            </select>
+          </div>
+
+          <div className="register-field">
+            <label className="register-label" htmlFor="abstractCategory">
+              Abstract Category:
+            </label>
+            <select
+              className="register-select"
+              id="abstractCategory"
+              value={abstractCategory}
+              onChange={(e) => setAbstractCategory(e.target.value)}
+            >
+              <option value="">Select Category</option>
+              <option value="Technology">Technology</option>
+              <option value="Science">Science</option>
+            </select>
+          </div>
+          <div className="register-field">
+            <label className="register-label" htmlFor="fullAddress">
+              Full Postal Address:
+            </label>
+            <textarea
+              className="register-textarea"
+              id="fullAddress"
+              value={fullAddress}
+              onChange={(e) => setFullAddress(e.target.value)}
+            />
+          </div>
+
+          <PaymentInfo />
+        </form>
+      </div>
+      <Footer />
+    </>
   );
 }
 
