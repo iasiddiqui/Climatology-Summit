0dd34c1c5fd271d074c2f43e031f8554ecdd8ed3
iasiddiqui <115731094+iasiddiqui@users.noreply.github.com>
2024-09-25 16:12:06 +0530

    add animation and fix home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { motion } from "framer-motion";
 import "./home.css";
 import Navbar from "../Navbar/navbar";
 import UpcomingConference from "./UpcomingConference/UpcomingConference";
@@ -7,7 +8,8 @@ import MediaPartners from "./MediaPartners/MediaPartners";
 import Faqs from "./Faqs/Faqs";
 import Footer from "./Footer/Footer";
 import ContentHero from "./ContentOfHero/ContentHero";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div className="home-main">
@@ -15,58 +17,75 @@ const Home = () => {
 
       {/* -------Content 1--------- */}
       <div className="video-content">
-        {/* <div className="video-bg">
-          <video autoPlay muted loop playsInline>
-            <source src="pictures/bg video.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div> */}
         <div className="content-1">
-          <div className="content-1-details">
+          <motion.div
+            className="content-1-details"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeIn" }}
+          >
             <h1 className="home-h1">Climatology Summit</h1>
             <h4 className="home-h4">Act Now, Save Tomorrow</h4>
-          </div>
-          <div className="register-button">
+
+            <h2 className="content1-h2">
+              Climate Change Conferences and Environmental Science Meetings
+            </h2>
+            <p className="content1-p">
+              We are pleased to announce the Climate Change and Environmental
+              Science Conference, centered on “Current Trends and Research in
+              Climate Change and Environmental Science.” This event will unite
+              prominent climate scientists, researchers, scholars, and industry
+              professionals from across the globe to share insights and present
+              their innovative research. The conference provides a distinctive
+              forum for exploring the latest developments and findings in
+              climate change and environmental science.
+            </p>
+          </motion.div>
+
+          <motion.div
+            className="register-button"
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, ease: "easeIn" }}
+          >
             <Link to="/register">
               <button>Register</button>
             </Link>
-          </div>
-          <div className="content1-pic">
-            {/* <img
-              className="content1-pic"
-              src="public/pictures/green_earth.png"
-              alt="Conference Image"
-            /> */}
-            <img src="public\pictures\environment-tree.png" alt="" />
-          </div>
+          </motion.div>
+
+          <motion.div
+            className="content1-pic"
+            initial={{ x: 100}}
+            animate={{ x: 0 }}
+            transition={{ duration: 1, ease: "easeIn" }}
+          >
+            <img src="public/pictures/environment-tree.png" alt="Tree" />
+          </motion.div>
         </div>
       </div>
+
       <div className="rectangle"></div>
 
       {/* --------Content  Hero---------- */}
-      <ContentHero />
+      {/* <ContentHero /> */}
 
       {/*----------- Upcoming Conferences------- */}
-
       <UpcomingConference />
 
       {/* ---------Climate Change--------- */}
-
       <ClimateChange />
 
       {/*-------- Media Partners---------------- */}
-
       <MediaPartners />
 
       {/*--------- FAQ'S---------------- */}
-
       <Faqs />
 
       {/* --------Footer------------ */}
-
       <Footer />
     </div>
   );
 };
 
 export default Home;
+
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import "./Navbar.css";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -28,92 +29,113 @@ const Navbar = () => {
 
   return (
     <>
-    <nav className={`navbar ${isScrolled ? "scrolled" : ""}`}>
-    <div className="nav-up-color"></div>
-      <div className="logo">
-        <img src="public/pictures/ClimateLogo.png" alt="Event Logo" />
-      </div>
-      <div className="menu-icon" onClick={toggleMenu}>
-        &#9776;
-      </div>
-      <ul className={isMenuOpen ? "active" : ""}>
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? "nav active-link" : "nav")}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li className="dropdown">
-          <NavLink
-            to="/upcoming-event"
-            className={({ isActive }) => (isActive ? "nav active-link" : "nav")}
-          >
-            Upcoming Events
-            <ArrowDropDownIcon />
-          </NavLink>
-          <ul className="dropdown-content">
+      <nav className={`navbar ${isScrolled ? "scrolled" : ""}`}>
+        <div className="nav-up-color"></div>
+        <div className="logo">
+          <img src="public/pictures/ClimateLogo.png" alt="Event Logo" />
+        </div>
+        <div className="menu-icon" onClick={toggleMenu}>
+          &#9776;
+        </div>
+        <div className="nav-links-container"> {/* Add this container */}
+          <ul className={isMenuOpen ? "active" : ""}>
             <li>
-              <NavLink className="dropdown-nav" to="/climate-change-conference">
-                Climate Change and Public Health Conference
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "nav active-link" : "nav"
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="dropdown">
+              <NavLink
+                to="/upcoming-event"
+                className={({ isActive }) =>
+                  isActive ? "nav active-link" : "nav"
+                }
+              >
+                Upcoming Events
+                <ArrowDropDownIcon />
+              </NavLink>
+              <ul className="dropdown-content">
+                <li>
+                  <NavLink
+                    className="dropdown-nav"
+                    to="/climate-change-conference"
+                  >
+                    Climate Change and Public Health Conference
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className="dropdown-nav"
+                    to="/green-technologies-conference"
+                  >
+                    Green Technologies and Renewable Energy Conference
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-nav" to="/ocean-health-conference">
+                    Ocean Health and Climate Dynamics Conference
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-nav" to="/biodiversity-conference">
+                    Climate Change and Biodiversity Conference
+                  </NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink
+                to="/venue"
+                className={({ isActive }) =>
+                  isActive ? "nav active-link" : "nav"
+                }
+              >
+                Venue
               </NavLink>
             </li>
             <li>
               <NavLink
-                className="dropdown-nav"
-                to="/green-technologies-conference"
+                to="/awards"
+                className={({ isActive }) =>
+                  isActive ? "nav active-link" : "nav"
+                }
               >
-                Green Technologies and Renewable Energy Conference
+                Awards
               </NavLink>
             </li>
             <li>
-              <NavLink className="dropdown-nav" to="/ocean-health-conference">
-                Ocean Health and Climate Dynamics Conference
+              <NavLink
+                to="/speaker-guidline"
+                className={({ isActive }) =>
+                  isActive ? "nav active-link" : "nav"
+                }
+              >
+                Speaker Guidelines
               </NavLink>
             </li>
             <li>
-              <NavLink className="dropdown-nav" to="/biodiversity-conference">
-                Climate Change and Biodiversity Conference
+              <NavLink
+                to="/contact"
+                className={({ isActive }) =>
+                  isActive ? "nav active-link" : "nav"
+                }
+              >
+                Contact
               </NavLink>
             </li>
           </ul>
-        </li>
-        <li>
-          <NavLink
-            to="/venue"
-            className={({ isActive }) => (isActive ? "nav active-link" : "nav")}
-          >
-            Venue
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/awards"
-            className={({ isActive }) => (isActive ? "nav active-link" : "nav")}
-          >
-            Awards
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/speaker-guidline"
-            className={({ isActive }) => (isActive ? "nav active-link" : "nav")}
-          >
-            Speaker Guidelines
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/contact"
-            className={({ isActive }) => (isActive ? "nav active-link" : "nav")}
-          >
-            Contact
-          </NavLink>
-        </li>
-        
-      </ul>
-    </nav>
+          <div className="nav-register-button">
+            <Link to="/register">
+              <button>Register</button>
+            </Link>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
