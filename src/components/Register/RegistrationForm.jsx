28daee3cd7506087cb3849d6a0549ed7bef11b1d
iasiddiqui <115731094+iasiddiqui@users.noreply.github.com>
2024-10-07 16:24:34 +0530

    register page

--- src/components/Register/RegistrationForm.jsx
+++ src/components/Register/RegistrationForm.jsx
@@ -9,32 +9,48 @@ const RegistrationForm = () => {
           <table className="registration-table">
             <tbody>
               <tr className="registration-row">
-                <td className="registration-category">Registration Type</td>
+                <td className="registration-category-head">
+                  Registration Type
+                </td>
 
-                <td className="registration-option" colSpan="2">
+                <td className="registration-option-head" colSpan="2">
                   Early Bird On/Before September 25, 2024
                 </td>
               </tr>
               <tr className="registration-row">
                 <td className="registration-category">Speaker Registration</td>
                 <td className="registration-option">
-                  <input type="checkbox" className="registration-checkbox" />{" "}
-                  Academia $799
+                  Academia
+                  <input
+                    type="checkbox"
+                    className="registration-checkbox"
+                  />{" "}
+                  $799
                 </td>
                 <td className="registration-option">
-                  <input type="checkbox" className="registration-checkbox" />{" "}
-                  Business $699
+                  {" "}
+                  Business
+                  <input
+                    type="checkbox"
+                    className="registration-checkbox"
+                  />{" "}
+                  $699
                 </td>
               </tr>
               <tr className="registration-row">
                 <td className="registration-category">Delegate</td>
                 <td className="registration-option">
-                  <input type="checkbox" className="registration-checkbox" />{" "}
-                  Academia $699
+                  {" "}
+                  Academia
+                  <input
+                    type="checkbox"
+                    className="registration-checkbox"
+                  />{" "}
+                  $699
                 </td>
                 <td className="registration-option">
                   <input type="checkbox" className="registration-checkbox" />{" "}
-                  Business $799
+                  $799
                 </td>
               </tr>
               <tr className="registration-row">
@@ -73,32 +89,51 @@ const RegistrationForm = () => {
           <table className="registration-table">
             <tbody>
               <tr className="registration-row">
-                <td className="registration-category">Registration Type</td>
+                <td className="registration-category-head">
+                  Registration Type
+                </td>
 
-                <td className="registration-option" colSpan="2">
+                <td className="registration-option-head" colSpan="2">
                   Next Round Registration Date: August 29, 2024
                 </td>
               </tr>
               <tr className="registration-row">
                 <td className="registration-category">Speaker Registration</td>
                 <td className="registration-option">
-                  <input type="checkbox" className="registration-checkbox" />{" "}
-                  Academia $899
+                  Academia
+                  <input
+                    type="checkbox"
+                    className="registration-checkbox"
+                  />{" "}
+                  $899
                 </td>
                 <td className="registration-option">
-                  <input type="checkbox" className="registration-checkbox" />{" "}
-                  Business $999
+                  Business
+                  <input
+                    type="checkbox"
+                    className="registration-checkbox"
+                  />{" "}
+                  $999
                 </td>
               </tr>
               <tr className="registration-row">
                 <td className="registration-category">Delegate</td>
                 <td className="registration-option">
-                  <input type="checkbox" className="registration-checkbox" />{" "}
-                  Academia $799
+                  {" "}
+                  Academia
+                  <input
+                    type="checkbox"
+                    className="registration-checkbox"
+                  />{" "}
+                  $799
                 </td>
                 <td className="registration-option">
-                  <input type="checkbox" className="registration-checkbox" />{" "}
-                  Business $899
+                  Business
+                  <input
+                    type="checkbox"
+                    className="registration-checkbox"
+                  />{" "}
+                  $899
                 </td>
               </tr>
               <tr className="registration-row">
@@ -137,32 +172,52 @@ const RegistrationForm = () => {
           <table className="registration-table">
             <tbody>
               <tr className="registration-row">
-                <td className="registration-category">Registration Type</td>
+                <td className="registration-category-head">
+                  Registration Type
+                </td>
 
-                <td className="registration-option" colSpan="2">
+                <td className="registration-option-head" colSpan="2">
                   Spot Registrations on July 14, 2024
                 </td>
               </tr>
               <tr className="registration-row">
                 <td className="registration-category">Speaker Registration</td>
                 <td className="registration-option">
-                  <input type="checkbox" className="registration-checkbox" />{" "}
-                  Academia $999
+                  {" "}
+                  Academia
+                  <input
+                    type="checkbox"
+                    className="registration-checkbox"
+                  />{" "}
+                  $999
                 </td>
                 <td className="registration-option">
-                  <input type="checkbox" className="registration-checkbox" />{" "}
-                  Business $1099
+                  Business
+                  <input
+                    type="checkbox"
+                    className="registration-checkbox"
+                  />{" "}
+                  $1099
                 </td>
               </tr>
               <tr className="registration-row">
                 <td className="registration-category">Delegate</td>
                 <td className="registration-option">
-                  <input type="checkbox" className="registration-checkbox" />{" "}
-                  Academia $899
+                  {" "}
+                  Academia
+                  <input
+                    type="checkbox"
+                    className="registration-checkbox"
+                  />{" "}
+                  $899
                 </td>
                 <td className="registration-option">
-                  <input type="checkbox" className="registration-checkbox" />{" "}
-                  Business $999
+                  Business
+                  <input
+                    type="checkbox"
+                    className="registration-checkbox"
+                  />{" "}
+                  $999
                 </td>
               </tr>
               <tr className="registration-row">
@@ -196,42 +251,107 @@ const RegistrationForm = () => {
             </tbody>
           </table>
         </div>
-
+      </div>
+      <div className="sponsor-div">
+        <h2 className="sponsor-title">Sponsor Registration</h2>
         <div className="sponsor-section">
-          <h2 className="sponsor-title">Sponsor Registration</h2>
           <table className="sponsor-table">
             <tbody>
-              <tr className="sponsor-row">
-                <td className="sponsor-category">
-                  <input type="checkbox" className="sponsor-checkbox" />{" "}
-                  Platinum Sponsor
+              <tr className="sponsor-names">
+                <td className="sponsor-name">
+                  <span className="sponsor-label">Platinum Sponsor</span>
                 </td>
-                <td className="sponsor-price">$7500</td>
+                <td className="sponsor-name">
+                  <span className="sponsor-label">Gold Sponsor</span>
+                </td>
+                <td className="sponsor-name">
+                  <span className="sponsor-label">Silver Sponsor</span>
+                </td>
+                <td className="sponsor-name">
+                  <span className="sponsor-label">Exhibitor</span>
+                </td>
               </tr>
-              <tr className="sponsor-row">
-                <td className="sponsor-category">
-                  <input type="checkbox" className="sponsor-checkbox" /> Gold
-                  Sponsor
+              <tr className="sponsor-prices">
+                <td className="sponsor-price">
+                  <input type="checkbox" className="sponsor-checkbox" />
+                  <span className="price-amount">$7500</span>
                 </td>
-                <td className="sponsor-price">$6000</td>
-              </tr>
-              <tr className="sponsor-row">
-                <td className="sponsor-category">
-                  <input type="checkbox" className="sponsor-checkbox" /> Silver
-                  Sponsor
+                <td className="sponsor-price">
+                  <input type="checkbox" className="sponsor-checkbox" />
+                  <span className="price-amount">$6000</span>
                 </td>
-                <td className="sponsor-price">$5000</td>
-              </tr>
-              <tr className="sponsor-row">
-                <td className="sponsor-category">
-                  <input type="checkbox" className="sponsor-checkbox" />{" "}
-                  Exhibitor
+                <td className="sponsor-price">
+                  <input type="checkbox" className="sponsor-checkbox" />
+                  <span className="price-amount">$5000</span>
                 </td>
-                <td className="sponsor-price">$3000</td>
+                <td className="sponsor-price">
+                  <input type="checkbox" className="sponsor-checkbox" />
+                  <span className="price-amount">$3000</span>
+                </td>
               </tr>
             </tbody>
           </table>
         </div>
+      </div>
+
+      <div className="accommodation">
+        <table className="accommodation-table">
+          <tbody>
+            <tr className="accommodation-row">
+              <td className="accommodation-category-head">
+                Accommodation Registration
+              </td>
+              <td className="accommodation-option-head">Single Occupancy</td>
+              <td className="accommodation-option-head">Double Occupancy</td>
+              <td className="accommodation-option-head">Triple Occupancy</td>
+            </tr>
+            <tr className="accommodation-row">
+              <td className="accommodation-category">For 2 Nights</td>
+              <td className="accommodation-option">
+                <input type="checkbox" className="accommodation-checkbox" />{" "}
+                $799
+              </td>
+              <td className="accommodation-option">
+                <input type="checkbox" className="accommodation-checkbox" />{" "}
+                $699
+              </td>
+              <td className="accommodation-option">
+                <input type="checkbox" className="accommodation-checkbox" />{" "}
+                $599
+              </td>
+            </tr>
+            <tr className="accommodation-row">
+              <td className="accommodation-category">For 3 Nights</td>
+              <td className="accommodation-option">
+                <input type="checkbox" className="accommodation-checkbox" />{" "}
+                $799
+              </td>
+              <td className="accommodation-option">
+                <input type="checkbox" className="accommodation-checkbox" />{" "}
+                $699
+              </td>
+              <td className="accommodation-option">
+                <input type="checkbox" className="accommodation-checkbox" />{" "}
+                $599
+              </td>
+            </tr>
+            <tr className="accommodation-row">
+              <td className="accommodation-category">For 5 Nights</td>
+              <td className="accommodation-option">
+                <input type="checkbox" className="accommodation-checkbox" />{" "}
+                $799
+              </td>
+              <td className="accommodation-option">
+                <input type="checkbox" className="accommodation-checkbox" />{" "}
+                $699
+              </td>
+              <td className="accommodation-option">
+                <input type="checkbox" className="accommodation-checkbox" />{" "}
+                $599
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
